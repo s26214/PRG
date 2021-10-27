20cf931c27a8aefa6d29d291c9831acec7a00758
s26214 <s26214@pjwstk.edu.pl>
2021-10-27 21:40:09 +0200

    loops

---
+++ src/s03-countdown.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+int liczba;
+auto main(int argc,char*argv[] ) -> int
+{
+auto const x = std::stoi(argv[1]);
+
+if(argc !=2){
+std::cout<<"wpisz 1 liczbę"<<"\n";
+return 1;
+}
+for (int i=x;i>=0; i--)
+{
+std::cout<<i<<"\n";
+}
+
+return 0;
+}
