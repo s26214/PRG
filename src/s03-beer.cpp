03c49476d1eeecdf2a14e906f90c79aad0b4de12
s26214 <91958480+s26214@users.noreply.github.com>
2021-11-03 17:55:46 +0100

    s03-beer.cpp
    loops

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -2,19 +2,20 @@
 #include <string>
 auto main(int, char*argv[])->int
 {
-int x;
-std::cin>>x;
-for(i=1;i>=x;i--)
-{
-if(i>0){
-//std::cout<<"99 bottles of beer on the wall, 99 bottles of beer.
-//Take one down, pass it around, 98 bottles of beer on the wall..
-}
-else {
-//No more bottles of beer on the wall, no more bottles of beer.
-//Go to the store and buy some more, 99 bottles of beer on the wall...
-}
-}
+    auto x = std::stoi(argv[1]);
+    for(int i=x;i>=0;i--)
+    {
+        if(i>1) {
+            std::cout << i << " bottles of beer on the wall,"<< i <<" bottles of beer.\n"<< "Take one down, pass it around, " << i-1<< " bottles of beer on the wall..\n";
+        }
+        else if (i==1){
+            std::cout << i << " bottle of beer on the wall,"<< i <<" bottle of beer.\n"<< "Take one down, pass it around, " << i-1<< " bottles of beer on the wall..\n";
+        }
+        else {
+            std::cout<<"No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more,"<< x <<" bottles of beer on the wall...\n";
+        }
 
-return 0;
+        }
+
+    return 0;
 }
