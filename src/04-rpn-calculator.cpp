f9c2852722e95302ee5e0f7812b30af2d47228d4
s26214 <s26214@pjwstk.edu.pl>
2021-11-09 23:25:46 +0100

    loops

--- src/04-rpn-calculator.cpp
+++ src/04-rpn-calculator.cpp
@@ -9,7 +9,7 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
-
+#include <cmath>
 
 auto pop_top(std::stack<double>& stack) -> double
 {
@@ -41,6 +41,76 @@ auto evaluate_subtraction(std::stack<double>& stack) -> void
     auto const a = pop_top(stack);
     stack.push(a - b);
 }
+auto evaluate_mul(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for *"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push(a * b);
+}
+
+auto evaluate_div(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for /"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push(a / b);
+}
+auto evaluate_div2(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for //"};
+    }
+    auto const  b = pop_top(stack);
+    auto const  a = pop_top(stack);
+    stack.push(std::round( a / b));	
+
+}
+auto evaluate_divr(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for %"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push( (int)a % (int)b);
+}
+auto evaluate_pow(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for **"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push( std::pow(a,b));
+}
+
+auto evaluate_sqrt(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 1) {
+        throw std::logic_error{"not enough operands for sqrt"};
+    }
+    auto const a = pop_top(stack);
+    stack.push(std::sqrt(a));
+}
+
+auto evaluate_cbrt(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 1) {
+        throw std::logic_error{"not enough operands for cbrt"};
+    }
+    auto const a = pop_top(stack);
+    stack.push(std::cbrt(a));
+}
+ 
+
+
+
+
 
 
 auto make_args(int argc, char* argv[]) -> std::vector<std::string>
@@ -63,9 +133,23 @@ auto main(int argc, char* argv[]) -> int
                 evaluate_addition(stack);
             } else if (each == "-") {
                 evaluate_subtraction(stack);
-            } else {
+            }else if (each == "*") {
+                evaluate_mul(stack);
+            } else if (each == "/") {
+                evaluate_div(stack);
+            } else if (each == "//") {
+                evaluate_div2(stack);
+            }  else if (each == "%") {
+                evaluate_divr(stack);
+            }else if (each == "**") {
+                evaluate_pow(stack);
+            }else if (each == "sqrt") {
+                evaluate_sqrt(stack);
+            } else if (each == "cbrt") {
+                evaluate_cbrt(stack);
+            }else {
                 stack.push(std::stod(each));
-            }
+		}
         } catch (std::logic_error const& e) {
             std::cerr << "error: " << each << ": " << e.what() << "\n";
         }
