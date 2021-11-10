ea64512436b45ec32057921885c8b11ce1aa7ad6
Kinga Wajda <s26214@pjwstk.edu.pl>
2021-11-10 16:30:29 +0100

    loops

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
 #include <string>
 
-auto main(int, char*argv)->int
+auto main(int argc, char* argv[]) -> int
 {
-auto const x = std::string{argv[]};
+    std::string x;
 
-return 0;
+    if (argv[1] == std::string("-n")) {
+        for (int i = 2; i < argc; i++) {
+            x = std::string(argv[i]);
+            std::cout << x << " ";
+        }
+
+    } else if (argv[1] == std::string("-r")) {
+        if (argv[2] == std::string("-l")) {
+            for (int i = argc - 1; i > 2; i--) {
+                x = std::string(argv[i]);
+                std::cout << x << "\n";
+            }
+
+        } else if (argv[2] == std::string("-n")) {
+            for (int i = argc - 1; i > 2; i--) {
+                x = std::string(argv[i]);
+                std::cout << x << " ";
+            }
+        } else {
+            for (int i = argc - 1; i > 1; i--) {
+                x = std::string(argv[i]);
+                std::cout << x << " ";
+
+                std::cout << "\n";
+            }
+        }
 }
+        else if (argv[1] == std::string("-l"))
+        {
+            for (int i = 2; i < argc; i++) {
+                x = std::string(argv[i]);
+                std::cout << x << "\n";
+            }
+        }
+        else
+        {
+            for (int i = 1; i < argc; i++) {
+                x = std::string(argv[i]);
+                std::cout << x << " ";
+            }
+            std::cout << "\n";
+        }
+
+
+        return 0;
+    }
