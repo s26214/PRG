3be83c54b1def3644fadc5f7383710b8f683f306
Kinga <91958480+s26214@users.noreply.github.com>
2021-12-15 17:19:50 +0100

    zadanie 1

---
+++ Exercises/Prime_Numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+auto Prime_numbers (int start_num, int num) -> std::string
+{ auto out = std::ostringstream{};
+
+        for (int i = start_num; i <= num; i++) {
+            int check = true;
+            for ( int j = 2; j <= sqrt(i); j++ ) {
+
+
+                if (i % j == 0) {
+                    check = false;
+
+                }
+            }
+            if (check == true){
+                out<< i <<" ";
+            }
+
+        }
+
+    return out.str();
+}
+ auto main() -> int
+ {
+    std::cout << Prime_numbers( 2, 20)<<"\n";
+     std::cout << Prime_numbers(2, 20);
+
+    return 0;
+}
