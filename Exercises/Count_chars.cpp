8c499a37ce346a83e1794ec987b83e4540b41dd9
Kinga <91958480+s26214@users.noreply.github.com>
2021-12-16 17:01:11 +0100

    zadanie 7

---
+++ Exercises/Count_chars.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+
+auto count_chars(std::string word, char letter) -> int
+{
+    auto num = 0;
+    for (int i = 0; i < word.size() ; i++ ){
+        if (word[i] == letter) {
+        num++;
+        }
+    }
+    return num;
+}
+
+
+
+ auto main() -> int
+ {
+
+    std::cout << count_chars("abrakadabra",'a');
+    return 0;
+}
