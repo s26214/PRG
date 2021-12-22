c707545fc63c53a95640c23c8b438f1e3db4f818
Kinga <91958480+s26214@users.noreply.github.com>
2021-12-22 15:57:50 +0100

    zadanie 2.1

---
+++ Exercises/average_of.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+auto average_of(std::vector<int> numbers) -> double
+{
+    double sum = 0;
+
+    for (auto i = 0; i <= numbers.size() - 1;++i) {
+        int  s_number = numbers[i];
+        sum += s_number;
+
+        }
+
+    return sum/2;
+
+}
+int main() {
+    auto vector = std::vector<int>{7,2,3,3,8};
+    std::cout<< average_of(vector);
+}
